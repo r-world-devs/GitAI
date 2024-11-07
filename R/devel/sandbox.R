0db4a400b2a67f56a71ca2be3379ba0527f88060
Kamil Wais <kamil.wais@gmail.com>
2024-11-07 14:56:53 +0100

    added readme

--- R/devel/sandbox.R
+++ R/devel/sandbox.R
@@ -3,10 +3,11 @@ pak::pak("tidyverse/elmer")
 
 chat <- elmer::chat_openai(
   model = "gpt-4o-mini",
-  system_prompt = "You are a friendly but terse assistant.",
+  # system_prompt = "You are a friendly but terse assistant.",
   echo = "none"
 )
 chat
+chat$system_prompt <- "You always start with 'HI'"
 chat |> str()
 result <- chat$chat("What is the meaning of life?")
 result
---
+++ README.md
@@ -0,0 +1,19 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# GitAI
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of GitAI is to …
+
+## Installation
+
+You can install the development version of GitAI from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("r-world-devs/GitAI")
+```
