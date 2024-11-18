1b5e6e6ef55ffd9f5928d211a93a954aa57e3958
Maciej Banas <maciej.banas@contractors.roche.com>
2024-11-18 12:13:48 +0000

    Adjust test.

--- tests/testthat/test-process_repos.R
+++ tests/testthat/test-process_repos.R
@@ -1,20 +1,20 @@
 test_that("process_repos() returns results with repo metadata", {
 
-  my_project <- 
-    initialize_project("gitai_test_project") |> 
+  my_project <-
+    initialize_project("gitai_test_project") |>
     set_github_repos(
       repos = c("r-world-devs/GitStats", "openpharma/DataFakeR")
     ) |>
-    add_files(file_paths = "README.md") |>
+    add_files(files = "README.md") |>
     set_llm() |>
-    set_prompt(system_prompt = "Summarize the user content if one sentence.") 
+    set_prompt(system_prompt = "Summarize the user content if one sentence.")
 
   my_project$gitstats$verbose_off()
-  
+
   results <- my_project |> process_repos()
-  
+
   results |> is.list() |> expect_true()
   results |> names() |> expect_equal(c("GitStats", "DataFakeR"))
-  
+
   results |> purrr::map(~ nchar(.x$text) > 10) |> unlist() |> all() |> expect_true()
 })
