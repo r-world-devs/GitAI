21cea3c85ba2e62b3c51a4163f18dfbf7826c786
Maciej Banas <maciej.banas@contractors.roche.com>
2024-11-27 09:20:31 +0000

    Add verbose control.

--- R/process_repos.R
+++ R/process_repos.R
@@ -1,28 +1,33 @@
 #' Run LLM on `GitAI` repositories content
 #' @name process_repos
 #' @param gitai A \code{GitAI} object.
+#' @param verbose A logical. If \code{FALSE} you won't be getting
+#' additional diagnostic messages.
 #' @return A list.
 #' @export
-process_repos <- function(gitai) {
+process_repos <- function(gitai, verbose = is_verbose()) {
 
   gitstats <- gitai$gitstats
 
   gitai$repos_metadata <-
     GitStats::get_repos(gitstats,
-                        add_contributors = FALSE)
+                        add_contributors = FALSE,
+                        verbose = verbose)
 
   GitStats::get_files_structure(
     gitstats_object = gitstats,
     pattern = gitai$files,
-    depth = 1L
+    depth = 1L,
+    verbose = verbose
   )
-  files_content <- GitStats::get_files_content(gitstats)
+  files_content <- GitStats::get_files_content(gitstats, verbose = verbose)
 
   results <-
     files_content$repo_name |>
     purrr::map(function(repo_name) {
-
-      cli::cli_alert_info("Processing repository: {.pkg {repo_name}}")
+      if (verbose) {
+        cli::cli_alert_info("Processing repository: {.pkg {repo_name}}")
+      }
 
       content_to_process <-
         files_content |>
