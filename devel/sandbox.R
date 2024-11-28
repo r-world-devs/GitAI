645773c6ea64d31bbada7772830528575b1f402d
Kamil Wais <kamil.wais@gmail.com>
2024-11-28 15:57:45 +0100

    updated sandbox

--- devel/sandbox.R
+++ devel/sandbox.R
@@ -1,2 +1,17 @@
 
-Sys.getenv("PINECONE_API_KEY")
+
+my_project <- 
+  initialize_project("gitai-demo") |>
+  set_github_repos(repos = c(
+    "r-world-devs/GitStats", 
+    "r-world-devs/GitAI", 
+    "openpharma/DataFakeR"
+  )) |>
+  add_files(files = "README.md") |>
+  set_llm() |>
+  set_prompt(paste(
+    "Write a paragraph of summary for a project based on given input.",
+    "Highlight business value of the project, its use cases and target audience."
+  ))
+
+process_repos(my_project)
