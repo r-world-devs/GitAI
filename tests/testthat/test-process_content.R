0bbd88122e219e616cf1c3e9eff042cf6a8a8863
Maciej Banas <maciej.banas@contractors.roche.com>
2024-11-28 10:32:41 +0000

    Add repo date to metadata.

--- tests/testthat/test-process_content.R
+++ tests/testthat/test-process_content.R
@@ -46,24 +46,3 @@ test_that("processing a single file content with deterministic output", {
 
   test_mocker$cache(result)
 })
-
-test_that("metadata is added to content", {
-  mocked_files_content <- dplyr::tibble(
-    repo_name = c("TestRepo", "TestRepo"),
-    repo_id = c("repo_id", "repo_id"),
-    organization = c("org", "org"),
-    file_path = c("file1.md", "file2.md"),
-    file_content = c("test1", "test2"),
-    file_size = c(1, 1),
-    repo_url = c("test_URL", "test_URL")
-  )
-  result_with_metadata <-
-    test_mocker$use("result") |>
-    add_metadata(
-      content = mocked_files_content
-    )
-  expect_true("metadata" %in% names(result_with_metadata))
-  expect_type(result_with_metadata[["metadata"]], "list")
-  expect_equal(names(result_with_metadata[["metadata"]]), c("repo_url", "files", "timestamp"))
-
-})
