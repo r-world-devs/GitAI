1828ce4b8fd653e81601e1450cf580c505846c32
Maciej Banas <maciej.banas@contractors.roche.com>
2024-12-11 11:42:08 +0000

    Add withr wrappers for verbose envar.

--- tests/testthat/test-set_repos.R
+++ tests/testthat/test-set_repos.R
@@ -1,15 +1,16 @@
 test_that("set_*_repos creates GitStats object inside GitAI with repos set", {
-  verbose_off()
-  my_project <- initialize_project("gitai_test_project")
+  withr::with_envvar(new = c("GITAI_VERBOSE" = FALSE), {
+    my_project <- initialize_project("gitai_test_project")
 
-  my_project <- my_project |>
-    set_github_repos(repos = c("r-world-devs/GitStats", "openpharma/DataFakeR"))
-  expect_true("GitStats" %in% class(my_project$gitstats))
+    my_project <- my_project |>
+      set_github_repos(repos = c("r-world-devs/GitStats", "openpharma/DataFakeR"))
+    expect_true("GitStats" %in% class(my_project$gitstats))
 
-  my_project <- my_project |>
-    set_gitlab_repos(repos = "mbtests/gitstatstesting")
-  expect_length(
-    my_project$gitstats$.__enclos_env__$private$hosts,
-    2
-  )
+    my_project <- my_project |>
+      set_gitlab_repos(repos = "mbtests/gitstatstesting")
+    expect_length(
+      my_project$gitstats$.__enclos_env__$private$hosts,
+      2
+    )
+  })
 })
