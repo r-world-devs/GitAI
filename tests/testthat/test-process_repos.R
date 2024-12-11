0c7ab89bbd77c74fd53acf4a0c9c144ca753ff8c
Maciej Banas <maciej.banas@contractors.roche.com>
2024-12-11 11:56:51 +0000

    Update after review, fix checks with setting withr in suggests.

--- tests/testthat/test-process_repos.R
+++ tests/testthat/test-process_repos.R
@@ -1,22 +1,21 @@
 test_that("process_repos() returns results with repo metadata", {
-  withr::with_envvar(new = c("GITAI_VERBOSE" = FALSE), {
-    my_project <- initialize_project("gitai_test_project") |>
-      set_github_repos(
-        repos = c("r-world-devs/GitStats", "openpharma/DataFakeR")
-      ) |>
-      add_files(files = "README.md") |>
-      set_llm() |>
-      set_prompt(system_prompt = "Summarize the user content if one sentence.")
-    results <- my_project |> process_repos()
+  withr::local_envvar(new = c("GITAI_VERBOSE" = FALSE))
+  my_project <- initialize_project("gitai_test_project") |>
+    set_github_repos(
+      repos = c("r-world-devs/GitStats", "openpharma/DataFakeR")
+    ) |>
+    add_files(files = "README.md") |>
+    set_llm() |>
+    set_prompt(system_prompt = "Summarize the user content if one sentence.")
+  results <- my_project |> process_repos()
 
-    expect_true(is.list(results))
-    expect_equal(c("GitStats", "DataFakeR"), names(results))
-    expect_true(
-      results |>
-        purrr::map(~ nchar(.x$text) > 10) |>
-        unlist() |>
-        all()
-    )
-    results |> purrr::walk(~ expect_true("metadata" %in% names(.)))
-  })
+  expect_true(is.list(results))
+  expect_equal(c("GitStats", "DataFakeR"), names(results))
+  expect_true(
+    results |>
+      purrr::map(~ nchar(.x$text) > 10) |>
+      unlist() |>
+      all()
+  )
+  results |> purrr::walk(~ expect_true("metadata" %in% names(.)))
 })
--- tests/testthat/test-set_repos.R
+++ tests/testthat/test-set_repos.R
@@ -1,16 +1,15 @@
 test_that("set_*_repos creates GitStats object inside GitAI with repos set", {
-  withr::with_envvar(new = c("GITAI_VERBOSE" = FALSE), {
-    my_project <- initialize_project("gitai_test_project")
+  withr::local_envvar(new = c("GITAI_VERBOSE" = FALSE))
+  my_project <- initialize_project("gitai_test_project")
 
-    my_project <- my_project |>
-      set_github_repos(repos = c("r-world-devs/GitStats", "openpharma/DataFakeR"))
-    expect_true("GitStats" %in% class(my_project$gitstats))
+  my_project <- my_project |>
+    set_github_repos(repos = c("r-world-devs/GitStats", "openpharma/DataFakeR"))
+  expect_true("GitStats" %in% class(my_project$gitstats))
 
-    my_project <- my_project |>
-      set_gitlab_repos(repos = "mbtests/gitstatstesting")
-    expect_length(
-      my_project$gitstats$.__enclos_env__$private$hosts,
-      2
-    )
-  })
+  my_project <- my_project |>
+    set_gitlab_repos(repos = "mbtests/gitstatstesting")
+  expect_length(
+    my_project$gitstats$.__enclos_env__$private$hosts,
+    2
+  )
 })
