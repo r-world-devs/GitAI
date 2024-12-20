bebe6737e7eb7ff3b0edf37b5d8f5bb1ec784dbd
Maciej Banas <maciej.banas@contractors.roche.com>
2024-12-20 15:20:41 +0000

    Some small adjustments with repo date.

--- R/process_repos.R
+++ R/process_repos.R
@@ -22,24 +22,24 @@ process_repos <- function(
     verbose = verbose
   )
   GitStats::get_files_structure(
-    gitstats_object = gitstats,
+    gitstats,
     pattern = paste0(gitai$files, collapse = "|"),
     depth = depth,
     verbose = verbose
   )
   files_content <- GitStats::get_files_content(gitstats, verbose = verbose)
 
-  distinct_repos <- files_content |> 
-    dplyr::distinct(repo_name, api_url) 
+  distinct_repos <- files_content |>
+    dplyr::distinct(repo_name, api_url)
 
   repositories <- distinct_repos$repo_name
   api_urls     <- distinct_repos$api_url
 
   results <-
     purrr::map2(repositories, api_urls, function(repo_name, api_url) {
-      
+
       current_repo_number <- which(repositories == repo_name)
-      
+
       if (verbose) {
         cli::cli_alert(paste0(
           "Processing repository ",
@@ -56,25 +56,24 @@ process_repos <- function(
         dplyr::pull(file_content) |>
         paste(collapse = "\n\n")
 
+      if (grepl("github", api_url)) {
+        api_url <- github_repo(api_url)
+      } else {
+        api_url <- gitlab_repo(api_url)
+      }
+      repo_timestamp <- get_repo_date(api_url)
+
       if (!is.null(gitai$db)) {
         if (verbose) {
           cli::cli_alert_info("Checking repo timestamp...")
         }
         record <- gitai$db$read_record(id = repo_name)
-        
+
         if (NROW(record) > 0) {
 
           record <- record[[1]]
           record_timestamp <- as.POSIXct(record$metadata$timestamp, tz = "UTC")
-          
-          if (grepl("github", api_url)) {
-            api_url <- github_repo(api_url)
-          } else {
-            api_url <- gitlab_repo(api_url)
-          }
-          
-          repo_timestamp <- get_repo_date(api_url)
-          
+
           if (repo_timestamp <= record_timestamp) {
             if (verbose) {
               cli::cli_alert_info("Repo has not been updated. Skipping...")
@@ -87,11 +86,13 @@ process_repos <- function(
       if (verbose) {
         cli::cli_alert_info("Processing content with LLM...")
       }
+
       result <- process_content(
         gitai = gitai,
-        content = content_to_process
+        content = content_to_process,
+        verbose = verbose
       ) |>
-        add_metadata(content = filtered_content)
+        add_metadata(content = filtered_content, timestamp = repo_timestamp)
 
       if (!is.null(gitai$db)) {
         if (verbose) {
--- tests/testthat/test-add_metadata.R
+++ tests/testthat/test-add_metadata.R
@@ -9,16 +9,13 @@ test_that("metadata is added to content", {
     repo_url = c("test_URL", "test_URL"),
     api_url = c("test_URL", "test_URL")
   )
-  testthat::with_mocked_bindings({
-    result_with_metadata <- "result" |>
-      test_mocker$use() |>
-      add_metadata(
-        content = mocked_files_content
-      )
-    expect_true("metadata" %in% names(result_with_metadata))
-    expect_type(result_with_metadata[["metadata"]], "list")
-    expect_equal(names(result_with_metadata[["metadata"]]), c("repo_url", "files", "timestamp"))
-  },
-  get_repo_date = function(api_url) Sys.time()
-  )
+  result_with_metadata <- "result" |>
+    test_mocker$use() |>
+    add_metadata(
+      content = mocked_files_content,
+      timestamp = Sys.Date()
+    )
+  expect_true("metadata" %in% names(result_with_metadata))
+  expect_type(result_with_metadata[["metadata"]], "list")
+  expect_equal(names(result_with_metadata[["metadata"]]), c("repo_url", "files", "timestamp"))
 })
