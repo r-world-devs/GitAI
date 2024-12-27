b6fdc4b7c411d59f3df471a905a5d01fe80c503a
Maciej Banas <maciej.banas@contractors.roche.com>
2024-12-27 10:42:06 +0000

    Add an example workflow.

--- inst/example_workflow.R
+++ inst/example_workflow.R
@@ -1,12 +1,15 @@
-# remotes::install_github("r-world-devs/GitStats@devel")
-
-gitai_demo <- initialize_project("gitai-demo-2") |>
-  set_database(index = "gitai-mb") |>
+gitai_demo <- initialize_project("gitai-tests") |>
+  set_database(index = "gitai-mb",
+               namespace = "gitai-demo-2") |>
   set_github_repos(
-    orgs = "pharmaverse"
+    orgs = "r-world-devs"
   ) |>
   add_files(files = "\\.md") |>
   set_llm() |>
   set_prompt("Provide a one-two sentence description of the product based on input.")
 
 process_repos(gitai_demo)
+
+gitai_demo$db$find_records("Find package with which I can plot data.")
+
+gitai_demo$db$read_record("GitStats")
